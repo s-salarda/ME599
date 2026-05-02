8fec097527e29c4493e5bb49669c2fc7a0164c64
Sarah Salarda <151248862+s-salarda@users.noreply.github.com>
2026-05-02 16:17:34 -0700

    finished

--- Assignment 2/assignment2_p1.py
+++ Assignment 2/assignment2_p1.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # File path to the DIOCOM
-file_path = 'path_to_your_dicom_file.dcm'
+file_path = 'CTA_hole_phantom.dcm'
 
 # Read the DICOM file
-dicom_data = pydicom.dcmread(file_path) 
-dcm.pixel_array
+dcm_data = pydicom.dcmread(file_path) 
+dcm_data.pixel_array
